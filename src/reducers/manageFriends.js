1ce7daabd88d2cc0275eb757bbeec11ebdf642cc
ianhunterharold <ianhunterharold@gmail.com>
2020-02-12 09:47:07 -0700

    working on an older lab to work through redux problem

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,29 +1,31 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+  switch(action.type) {
 
-  state = {
-    "friends": [
-            {
-            "hometown": "NYC",
-            "id": 100,
-            "name": "Avi"
-            }
-        ]
+    case "ADD_FRIEND":
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    )
+
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
+    default:
+      return state;
+
   }
-
-  switch(action.type){ 
-
-    case 'ADD_FRIEND':
-      return ({ ...state, friends: [...state.friends, action.friend] })
-
-    case 'REMOVE_FRIEND':
-      const removeIndex = state.friends.findIndex(friend => friend.id === action.id)
-
-      return( {...state, friends: [...state.friends.slice(0, removeIndex),  ...state.friends.slice(removeIndex + 1)] } )
-
-      default: 
-        return state
-  }
-  
-
-
-}
+};
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -10,6 +10,4 @@ export function managePresents(state, action){
     return state;
   } 
 
-
-
 }
